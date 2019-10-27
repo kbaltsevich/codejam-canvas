0bc3e5648c13232608e6191e053da1e7d350de94
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2019-10-27 14:09:21 +0300

    feat: delete files

--- codejam-canvas/assets/data/four.js
+++
@@ -1,8 +0,0 @@
-const arr_4x4 = [
-    ["#00BCD4", "#FFEB3B","#FFEB3B","#00BCD4"],
-    ["#FFEB3B", "#FFC107","#FFC107","#FFEB3B"],
-    ["#FFEB3B", "#FFC107","#FFC107","#FFEB3B"],
-    ["#00BCD4", "#FFEB3B","#FFEB3B","#00BCD4"]
-];
-
-module.exports = arr_4x4;
