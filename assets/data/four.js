9f8905fce2dbd1cf2e7b06b7df2779fa91ad987d
Kanstantsin Baltsevich <kostielgreendevil@gmail.com>
2019-10-27 13:51:22 +0300

    feat: add new branch gh-pages

---
+++ assets/data/four.js
@@ -0,0 +1,8 @@
+const arr_4x4 = [
+    ["#00BCD4", "#FFEB3B","#FFEB3B","#00BCD4"],
+    ["#FFEB3B", "#FFC107","#FFC107","#FFEB3B"],
+    ["#FFEB3B", "#FFC107","#FFC107","#FFEB3B"],
+    ["#00BCD4", "#FFEB3B","#FFEB3B","#00BCD4"]
+];
+
+module.exports = arr_4x4;
